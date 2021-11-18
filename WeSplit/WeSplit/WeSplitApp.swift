9efb31e172855f6b180bb75f4c950e9ce4a550b2
Alexander <56388642+AlexanderRozhdestvenskiy@users.noreply.github.com>
2021-11-18 16:30:00 +0300

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alexander Rozhdestvenskiy on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
