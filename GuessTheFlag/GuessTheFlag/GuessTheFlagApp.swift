3a6523e6284b4ac8af2030a7032afd03d79bb35f
Alexander <56388642+AlexanderRozhdestvenskiy@users.noreply.github.com>
2021-11-22 14:23:57 +0300

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alexander Rozhdestvenskiy on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
