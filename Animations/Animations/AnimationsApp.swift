fa046c11a45f211edbec850923c4a3770f7d2261
Alexander <56388642+AlexanderRozhdestvenskiy@users.noreply.github.com>
2021-11-26 20:22:38 +0300

    Add files via upload

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Alexander Rozhdestvenskiy on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Snape()
+        }
+    }
+}
