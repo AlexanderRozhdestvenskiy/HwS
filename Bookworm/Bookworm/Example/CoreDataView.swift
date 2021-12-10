89c4949e27e5914c6a7766106c7da358ad0c76e9
Alexander <56388642+AlexanderRozhdestvenskiy@users.noreply.github.com>
2021-12-10 18:16:35 +0300

    Add files via upload

---
+++ Bookworm/Bookworm/Example/CoreDataView.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataView.swift
+//  Bookworm
+//
+//  Created by Alexander Rozhdestvenskiy on 10.12.2021.
+//
+
+import SwiftUI
+
+struct CoreDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoreDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoreDataView()
+    }
+}
